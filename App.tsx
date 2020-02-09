8565275401118212e91cf6c48c9dc3ef6ed62078
ria <ria@iMac.local>
2020-02-09 16:46:39 +0900

    nativebase の theme を eject

--- App.tsx
+++ App.tsx
@@ -1,13 +1,19 @@
 import React from 'react'
 import { ThemeProvider } from 'emotion-theming'
+import { StyleProvider } from 'native-base'
+
+import getTheme from './native-base-theme/components'
+import commonColor from './native-base-theme/variables/commonColor'
 
 import { theme } from './src/theme'
 import { Home } from './src/pages'
 
 export default function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
+    <StyleProvider style={getTheme(commonColor)}>
+      <ThemeProvider theme={theme}>
+        <Home />
+      </ThemeProvider>
+    </StyleProvider>
   )
 }
